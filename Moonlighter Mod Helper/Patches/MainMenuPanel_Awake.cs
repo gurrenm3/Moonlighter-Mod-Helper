311e7742077927bdd60a93f1695c38f2fcf87cab
Thomas <thomaspmar@protonmail.com>
2021-03-17 16:00:37 -0700

    Add project files.

---
+++ Moonlighter Mod Helper/Patches/MainMenuPanel_Awake.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace Moonlighter_Mod_Helper.Patches
+{
+    [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.Awake))]
+    internal class MainMenuPanel_Enable
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(MainMenuPanel __instance)
+        {
+
+        }
+    }
+}
