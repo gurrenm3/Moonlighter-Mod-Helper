f3f5af3b82b16a5309dba87f81db669c866124bb
Thomas <thomaspmar@protonmail.com>
2021-03-27 11:45:27 -0700

    Added access to the BepInEx Logger

--- Moonlighter Mod Helper/Main.cs
+++ Moonlighter Mod Helper/Main.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
 using Moonlighter_Mod_Helper.Api.Web;
 using System;
@@ -14,12 +15,14 @@ namespace Moonlighter_Mod_Helper
     [BepInPlugin("GurrenM4.Moonlighter_Mod_Helper", "Moonlighter Mod Helper", "1.0.0")]
     public class Main : BaseUnityPlugin
     {
+        internal static Main instance;
         internal static Assembly modAssembly = Assembly.GetExecutingAssembly();
         internal static string modName = $"{modAssembly.GetName().Name}";
         internal static string modDir = $"{Environment.CurrentDirectory}\\BepInEx\\{modName}";
 
         void Awake()
         {
+            instance = this;
             new Harmony($"GurrenM4_{modName}").PatchAll(modAssembly);
             Logger.LogInfo($"{modName} has loaded");
         }
@@ -27,6 +30,21 @@ namespace Moonlighter_Mod_Helper
         void Update()
         {
             
+        }
+
+        internal static void LogMessage(string message)
+        {
+            instance.Logger.LogMessage(message);
+        }
+
+        internal static void LogError(string message)
+        {
+            instance.Logger.LogError(message);
+        }
+
+        internal static void Log(LogLevel level, string message)
+        {
+            instance.Logger.Log(level, message);
         }
     }
 }
