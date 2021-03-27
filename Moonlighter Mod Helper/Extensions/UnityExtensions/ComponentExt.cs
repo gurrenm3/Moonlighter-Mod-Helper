750bf78e8148a94e8837fc9dc3bd2b1c95b9a34d
Thomas <thomaspmar@protonmail.com>
2021-03-27 11:45:06 -0700

    Created UnityExtensions

---
+++ Moonlighter Mod Helper/Extensions/UnityExtensions/ComponentExt.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Moonlighter_Mod_Helper.Extensions
+{
+    public static class ComponentExt
+    {
+        public static T GetComponent<T>(this Component gameObject, string componentPath) where T : Component
+        {
+            return gameObject.transform.Find(componentPath).GetComponent<T>();
+        }
+
+        public static void RemoveComponent<T>(this Component gameObject) where T : Component
+        {
+            GameObject.Destroy(gameObject.GetComponent<T>());
+        }
+
+        public static void RemoveComponent<T>(this Component gameObject, Component component) where T : Component
+        {
+            GameObject.Destroy(component);
+        }
+
+        public static void RemoveComponentByPath<T>(this Component gameObject, string componentPath) where T : Component
+        {
+            GameObject.Destroy(gameObject.transform.Find(componentPath));
+        }
+
+        public static void RemoveComponentByName(this Component gameObject, string componentName)
+        {
+            GameObject.Destroy(gameObject.GetComponent(componentName));
+        }
+    }
+}
---
+++ Moonlighter Mod Helper/Extensions/UnityExtensions/GameObjectExt.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Moonlighter_Mod_Helper.Extensions
+{
+    public static class GameObjectExt
+    {
+        public static T GetComponent<T>(this GameObject gameObject, string componentPath) where T : Component
+        {
+            return gameObject.transform.Find(componentPath).GetComponent<T>();
+        }
+
+        public static void RemoveComponent<T>(this GameObject gameObject) where T : Component
+        {
+            GameObject.Destroy(gameObject.GetComponent<T>());
+        }
+
+        public static void RemoveComponent<T>(this GameObject gameObject, Component component) where T : Component
+        {
+            GameObject.Destroy(component);
+        }
+
+        public static void RemoveComponentByPath<T>(this GameObject gameObject, string componentPath) where T : Component
+        {
+            GameObject.Destroy(gameObject.transform.Find(componentPath));
+        }
+
+        public static void RemoveComponentByName(this GameObject gameObject, string componentName)
+        {
+            GameObject.Destroy(gameObject.GetComponent(componentName));
+        }
+    }
+}
